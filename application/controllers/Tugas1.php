fe25e0a312358be56065c57f5fa3fc2d748c10bd
sadillaevrillia18 <evrilliasadilla4@gmail.com>
2024-05-02 11:07:24 +0700

    first commit

---
+++ application/controllers/Tugas1.php
@@ -0,0 +1,12 @@
+
+<?php
+class tugas1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Sadilla Evrillia
+ Saya tinggal di daerah Ambawang 
+ berhasal dari Singkawang";
+ }
+}
